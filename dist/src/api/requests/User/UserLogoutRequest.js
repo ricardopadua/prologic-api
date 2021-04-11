31d436f9c0ab3ffff60545e3ca62516a3a298a3a
rickp <ricardo.padua4@gmail.com>
2021-04-10 21:45:30 -0300

    add dist gitignore

--- dist/src/api/requests/User/UserLogoutRequest.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class UserLogoutRequest {
-    constructor() { }
-}
-exports.default = UserLogoutRequest;
-//# sourceMappingURL=UserLogoutRequest.js.map
